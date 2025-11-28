924d3a96ab17d48ddf6bafbcc1391433355d4b66
Pranay Junare <junarepranay@gmail.com>
2025-11-27 18:31:26 -0600

    minimal websocket server

---
+++ VRApp/README.md
@@ -0,0 +1,7 @@
+## Installation
+
+1. Create the conda environment
+    ```bash
+    conda env create -f env.yaml
+    conda activate nirvana
+    ```
---
+++ server.py
@@ -0,0 +1,29 @@
+import asyncio
+import websockets
+RECEIVER_IP = "100.70.51.33" # my local IP address
+RECEIVER_PORT = 8765
+
+async def handler(websocket):
+    """
+    A simple handler that waits for messages (values) and prints them.
+    """
+    print(f"A client (your Quest) connected!")
+    
+    try:
+        # This loop waits forever for new messages from this client
+        async for message in websocket:
+            # 'message' is the value you sent from Unity.
+            print(f"[Client says]: {message}")
+            
+    except websockets.exceptions.ConnectionClosed:
+        print("Client disconnected.")
+
+async def main():
+    # Start the WebSocket server and run it forever
+    async with websockets.serve(handler, RECEIVER_IP, RECEIVER_PORT):
+        print(f"Minimal server started at {RECEIVER_IP}:{RECEIVER_PORT}...")
+        print("Waiting for a client (your Quest) to connect...")
+        await asyncio.Future()  # Run forever
+
+if __name__ == "__main__":
+    asyncio.run(main())
